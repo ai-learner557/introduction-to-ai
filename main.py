db025d9afdb1d786444ce31a7a4457b15661b7f6
ai-learner557 <hakimajarmoumi@gmail.com>
2025-10-31 16:42:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello my name is chatbot ai what ur name?")
+name=input("Name:")
+print(f"nice to meet you {name} ")
+print("how are you felling today")
+feeling=input("enter how you feel")
+if feeling=="good":
+    print(" thats good im happy for you")
+else:
+    print("u should be more happy!")
+print("nice chating with you goodbye")
